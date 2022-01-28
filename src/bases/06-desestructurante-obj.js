8574785d72f4481692c30cf673bb11a1b601161e
Santiago <santiago.beltramo@live.com.ar>
2022-01-28 00:32:03 -0300

    problemas con useContext y useState

---
+++ src/bases/06-desestructurante-obj.js
@@ -0,0 +1,37 @@
+// Desestructuración o Asignacion desestructurante
+
+
+const persona = {
+    nombre: 'Santiago',
+    edad: 29,
+    profesion: 'Programador',
+    rango: 'trainee/jr',
+}
+
+
+// Forma incorrecta
+// console.log(persona.nombre);
+// console.log(persona.edad);
+// console.log(persona.profesion);
+
+//formas correctas
+
+const {nombre} = persona;
+console.log(nombre);
+
+const retornaPersona = ({ edad, profesion, rango}) => {
+
+    console.log(edad,profesion, rango); 
+}
+
+retornaPersona (persona);   
+
+const useContext  = ({ edad,rango}) => {
+
+    return {
+        nombreRango: rango,
+        anios: edad,
+        }
+}
+const {nombreRango, anios} = useContext(persona);
+console.log(nombreRango,anios);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
+// Desestructurante de arreglos
+// forma incorrecta
 
-console.log('');
+const personas = ['santiago',29,'gonzalo', 27, 'ignacio',22];
+console.log(personas);
+//  console.log(arreglo[0]);    
+//  console.log(arreglo[1]);
+//  console.log(arreglo[2]);
+//  console.log(arreglo[3]);
+//  console.log(arreglo[4]);
+//  console.log(arreglo[5]);
+
+//formas correctas 
+
+const [ , , p2,n2] = personas;
+console.log(p2,n2);
+
+
+const retornaArreglo = () => {
+    return ['NEWELLS','central']
+}
+const [equipo1,equipo2] = retornaArreglo();
+console.log(equipo1,equipo2);
+
+const useState = (valor) => {
+    return [valor,  () =>{console.log('hola mundo')} ]
+}
+const [nombre,setNombre] = useState('NOB');
+console.log([nombre,setNombre])
+setNombre();
